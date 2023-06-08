fe534713340cfc090a5f7f25527b0f983f3e807a
Abdullah Saeed <bnbshrb80@gmail.com>
2023-06-08 18:39:56 +0300

    Add the folder

---
+++ Whatsapp_sender/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Whatsapp sender',
+    'version': '1.0',
+    'author': 'Abdullah saeed',
+    'summary': 'Whatsapp sender, is a module for send whatsapp message in odoo.',
+    'description': """
+    Whatsapp sender, is a module for send whatsapp message in odoo, and you can make a lot of templet or custom it.
+    """,
+    'depends': ['hr'],
+    'data': [
+        "security/ir.model.access.csv",
+        "wizard/wizard.xml",
+        "data/type_records.xml",
+        "data/temp_records.xml",
+        "views/inherit_employees.xml",
+        "views/inherit_contact.xml",
+        "views/temp_edit.xml",
+        "views/type_temp_edit.xml",
+        "views/menuitems.xml",
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
